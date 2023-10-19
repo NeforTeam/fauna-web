d0e4a645bccca969402357e4163e67ef695daa06
ProdamGaraj <prodgaraj@yandex.ru>
2023-10-19 19:42:56 +0300

    length of respone now is unlimited

--- perplexity_api.py
+++ perplexity_api.py
@@ -2,7 +2,7 @@ from perplexity import Perplexity
 import re
 
 class Pyplapi:
-    async def get_description(object_name: str, length: int ):
+    async def get_description(object_name: str):
         perplexity = Perplexity("baracuda000330@gmail.com")
         answer = perplexity.search(f"Give me wikipedia description of {object_name} do not say it`s wikipedia, just give me description text only")
         responce = ""
