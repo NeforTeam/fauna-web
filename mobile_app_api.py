2871ee5c284ce07b4a17e708a8ca966b620c5811
ProdamGaraj <prodgaraj@yandex.ru>
2023-10-19 19:40:56 +0300

    fast api backend added

--- mobile_app_api.py
+++ mobile_app_api.py
@@ -1,21 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
-import wiki_api as wiki_api
-from pydantic import BaseModel
-
-app = FastAPI()
-
-
-class AnimalShortModel(BaseModel):
-    name: str
-    photo_link: str
-    wiki_link: str
-
-class AnimalBigModel(BaseModel):
-    name: str
-    photo_link: str
-    wiki_link: str
-    description: str
+from perplexity_api import Pyplapi as papi
+import models
 
 app = FastAPI()
 
@@ -23,17 +9,13 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/get_animal_short_model")
-def send_photo(animal_name: str):
-    # goto ai and get name of animal
-
-    # goto wiki and get animal photo link
-    return {}
+@app.get("/get_animal_short")
+async def get_animal_model_short(object_name: str):
+    responce = await papi.get_description(object_name)
+    return responce
 
 
-@app.post("/get_animal_big_model")
-def send_photo(animal_name: str):
-    # goto wiki and get animal description
-
-    # goto CHAT api and get better descr
-    return wiki_api.get_wiki_description()
+@app.get("/get_animal_long")
+async def get_animal_model_long(object_name: str):
+    responce = await papi.get_description(object_name)  
+    return responce
