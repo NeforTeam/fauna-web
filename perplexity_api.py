14a5e1c609cc65f9e13669ec40359973145401b6
ProdamGaraj <prodgaraj@yandex.ru>
2023-10-19 19:40:42 +0300

    perplexity request added

---
+++ perplexity_api.py
@@ -0,0 +1,15 @@
+from perplexity import Perplexity
+import re
+
+class Pyplapi:
+    async def get_description(object_name: str, length: int ):
+        perplexity = Perplexity("baracuda000330@gmail.com")
+        answer = perplexity.search(f"Give me wikipedia description of {object_name} do not say it`s wikipedia, just give me description text only")
+        responce = ""
+        for a in answer:
+            if a["answer"] is not None:
+               responce= a["answer"]
+        perplexity.close()
+        responce = re.sub(r'\[[^\]]*\]|\n\d*', '', responce)
+        return responce
+        
